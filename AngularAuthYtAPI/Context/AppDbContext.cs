330ef7f0287c727b38d85b2959d81ed4491ea4d4
JayanikaGithub <AR96588@fhss.sjp.ac.lk>
2023-12-18 17:57:04 +0530

    First Commit
    2023-12-18

---
+++ AngularAuthYtAPI/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AngularAuthYtAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularAuthYtAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+
+    }
+}
